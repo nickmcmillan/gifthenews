1e1d310a67d563681807b561fc9f5a10c958c11f
Nick McMillan <nick.a.mcmillan@gmail.com>
2016-05-30 11:38:35 +1000

    striphtml - changed to using regex to prevent tracking images from being fired from reuters feed

--- src/components/strip-html.js
+++ src/components/strip-html.js
@@ -1,6 +1,14 @@
-
+// do it using regex, not createElement (below), otherwise it renders dom elements within the feed, and those can cause unwanted network requests
 export default function stripHTML(html) {
-   var tmp = document.createElement("DIV");
-   tmp.innerHTML = html;
-   return tmp.textContent.trim() || tmp.innerText.trim() || "";
+  if (html) {
+    return html.replace(/<(?:.|\n)*?>/gm, '').trim();
+  }
 }
+
+
+//
+// export default function stripHTML(html) {
+//    var tmp = document.createElement("DIV");
+//    tmp.innerHTML = html;
+//    return tmp.textContent.trim() || tmp.innerText.trim() || "";
+// }
